ba95248e61d62c47674833f7aca2cac127f8b2ae
SohanaN <sohananaz01@gmail.com>
2022-03-26 19:48:10 +0600

    cart css added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './Cart.css';
 const Cart = ({cart}) => {
     const {name} = cart;
     return (
-        <div>
+        <div className='cart'>
              <h2>Selected Items: {cart.length}</h2>
                 <p>{name}</p>
                 <button>Choose 1 item for me</button>
